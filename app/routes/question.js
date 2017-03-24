2079d2cb335b4dbdca8ee021677f154ec379132e
Katie Reed <katharynreedpierce@gmail.com>
2017-03-24 10:07:40 -0700

    create questions route build

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    createAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('question');
+    },
+
+    destroyAnswer(answer) {
+      answer.destroyRecord();
+      this.transitionTo('question');
+    }
+  }
 });
