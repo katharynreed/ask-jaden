96c5fd631d66a8851898ce0f2ae097559e1549f5
Katie Reed <katharynreedpierce@gmail.com>
2017-03-24 09:34:17 -0700

    add answer to model

---
+++ app/models/answer.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  answer: DS.attr('string'),
+  author: DS.attr('string'),
+  image: DS.attr('string'),
+  question: DS.belongsTo('question', {async: true}),
+});
